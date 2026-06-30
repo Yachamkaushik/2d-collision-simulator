d5ad69e9fa6b273a73cfdb7fc94946476e9292e1
Kaushik <yachamkaushik@gmail.com>
2026-06-30 15:10:11 +0530

    ball movement and wall collisions added

--- main.py
+++ main.py
@@ -6,12 +6,26 @@ clock=pygame.time.Clock()
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
+x=100
+y=100
+vx = 4
+vy=2
 while run:
     clock.tick(60)
+    if x>=775:
+        vx *= -1
+    if x<=25 :
+        vx *= -1
+    if y>=775:
+        vy *= -1
+    if y<=25 :
+        vy *= -1
+    x+=vx
+    y+=vy
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
     window.fill(WHITE)
-    pygame.draw.circle(window,RED,(100,100),10)
+    pygame.draw.circle(window,RED,(x,y),25)
     pygame.display.flip()
 pygame.quit()
