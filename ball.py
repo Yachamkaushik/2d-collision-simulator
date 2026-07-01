f53d8f70fc898a7d25bf072d6b87d7a82be2ef34
Kaushik <yachamkaushik@gmail.com>
2026-07-01 11:52:54 +0530

    Created ball class

---
+++ ball.py
@@ -0,0 +1,23 @@
+import pygame
+class Ball:
+    def __init__(self,x,y,vx,vy,color,radius,mass):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.color = color
+        self.radius = radius
+        self.mass = mass
+    def draw(self,window):
+        pygame.draw.circle(window,self.color,(self.x,self.y),self.radius)
+    def update(self,height,width):
+        if self.x>=width-self.radius:
+            self.vx*=-1
+        if self.x<=self.radius:
+            self.vx*=-1
+        if self.y>=height-self.radius:
+            self.vy*=-1
+        if self.y<=self.radius:
+            self.vy*=-1
+        self.x += self.vx
+        self.y += self.vy
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import ball
 pygame.init()
 window =pygame.display.set_mode((800,800))
 run=True
@@ -6,26 +7,18 @@ clock=pygame.time.Clock()
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
-x=100
-y=100
-vx = 4
-vy=2
+ball1=ball.Ball(100,100,5,6,BLUE,20,10)
+ball2=ball.Ball(100,100,3,9,RED,20,100)
 while run:
+    size = window.get_size()
     clock.tick(60)
-    if x>=775:
-        vx *= -1
-    if x<=25 :
-        vx *= -1
-    if y>=775:
-        vy *= -1
-    if y<=25 :
-        vy *= -1
-    x+=vx
-    y+=vy
+    ball1.update(size[1],size[0])
+    ball2.update(size[1], size[0])
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
     window.fill(WHITE)
-    pygame.draw.circle(window,RED,(x,y),25)
+    ball1.draw(window)
+    ball2.draw(window)
     pygame.display.flip()
 pygame.quit()
